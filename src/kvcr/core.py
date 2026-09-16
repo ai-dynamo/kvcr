55d0bb3cc0413fa2e215cf85e8a963ce28fe02df
Moein Khazraee <moein@nvidia.com>
2026-09-16 10:12:45 -0700

    Reduce metadata lock hold time on caller and progress threads
    Signed-off-by: Moein Khazraee <moein@nvidia.com>

--- src/kvcr/core.py
+++ src/kvcr/core.py
@@ -59,15 +59,6 @@ _RecordDuration = Callable[[str, float | None, str], None]
 _RecordTransfer = Callable[[str, float | None, bool, int, int], None]
 
 
-def _with_state_lock(method: Callable) -> Callable:
-    @functools.wraps(method)
-    def locked(self: "_KVCRCore", *args, **kwargs):
-        with self._state_lock:
-            return method(self, *args, **kwargs)
-
-    return locked
-
-
 def _noop_timer() -> None:
     return None
 
@@ -382,7 +373,6 @@ class _KVCRCore:
         return statuses
 
     # TODO: Add optional completion callbacks to movement APIs.
-    @_with_state_lock
     def deliver(
         self,
         blocks: Mapping[BlockKey, list[MemDescriptor]],
@@ -396,38 +386,38 @@ class _KVCRCore:
             key: self._normalize_descriptors(descriptors)
             for key, descriptors in blocks.items()
         }
-        local_blocks: dict[BlockKey, list[MemDescriptor]] = {}
-        g3_blocks: dict[BlockKey, MemDescriptor] = {}
-        remote_blocks: dict[BlockKey, list[MemDescriptor]] = {}
-        for key, destination in normalized.items():
-            if self._is_local_resident(key):
-                local_blocks[key] = destination
-            elif self._g3 is not None and self._g3.is_ready(key):
-                g3_blocks[key] = destination[0]
-            else:
-                remote_blocks[key] = destination
+        with self._state_lock:
+            local_blocks: dict[BlockKey, list[MemDescriptor]] = {}
+            g3_blocks: dict[BlockKey, MemDescriptor] = {}
+            remote_blocks: dict[BlockKey, list[MemDescriptor]] = {}
+            for key, destination in normalized.items():
+                if self._is_local_resident(key):
+                    local_blocks[key] = destination
+                elif self._g3 is not None and self._g3.is_ready(key):
+                    g3_blocks[key] = destination[0]
+                else:
+                    remote_blocks[key] = destination
 
-        if sum(map(bool, (local_blocks, g3_blocks, remote_blocks))) > 1:
-            self._joined_completions[op_handle] = (set(blocks), {})
-        if local_dram is not None and local_blocks:
-            local_dram.deliver(op_handle, local_blocks, deadline=deadline)
-        if g3_blocks and (
-            self._g3 is None
-            or not self._g3.start_deliver(op_handle, g3_blocks, deadline)
-        ):
-            self._complete(
-                op_handle,
-                {key: OpEntryResult(OpEntryStatus.FAILED) for key in g3_blocks},
-            )
-        if remote_blocks:
-            self._remote_fw_dram.deliver(
-                op_handle, remote_blocks, request_id, deadline=deadline
-            )
-        elif not blocks:
-            self._complete(op_handle, {})
+            if sum(map(bool, (local_blocks, g3_blocks, remote_blocks))) > 1:
+                self._joined_completions[op_handle] = (set(blocks), {})
+            if local_dram is not None and local_blocks:
+                local_dram.deliver(op_handle, local_blocks, deadline=deadline)
+            if g3_blocks and (
+                self._g3 is None
+                or not self._g3.start_deliver(op_handle, g3_blocks, deadline)
+            ):
+                self._complete(
+                    op_handle,
+                    {key: OpEntryResult(OpEntryStatus.FAILED) for key in g3_blocks},
+                )
+            if remote_blocks:
+                self._remote_fw_dram.deliver(
+                    op_handle, remote_blocks, request_id, deadline=deadline
+                )
+            elif not blocks:
+                self._complete(op_handle, {})
         return op_handle
 
-    @_with_state_lock
     def deposit(
         self,
         blocks: Mapping[BlockKey, list[MemDescriptor]],
@@ -436,24 +426,26 @@ class _KVCRCore:
     ) -> OpHandle:
         op_handle = self._next_op_handle
         self._next_op_handle += 1
-        if self._local_dram is None:
-            self._complete(
-                op_handle,
-                {key: OpEntryResult(OpEntryStatus.FAILED) for key in blocks},
-            )
-        else:
-            self._local_dram.deposit(
-                op_handle,
-                {
-                    key: self._normalize_descriptors(descriptors)
-                    for key, descriptors in blocks.items()
-                },
-                no_evict=no_evict,
-                hints=hints,
-            )
+        if self._local_dram is not None:
+            blocks = {
+                key: self._normalize_descriptors(descriptors)
+                for key, descriptors in blocks.items()
+            }
+        with self._state_lock:
+            if self._local_dram is None:
+                self._complete(
+                    op_handle,
+                    {key: OpEntryResult(OpEntryStatus.FAILED) for key in blocks},
+                )
+            else:
+                self._local_dram.deposit(
+                    op_handle,
+                    blocks,
+                    no_evict=no_evict,
+                    hints=hints,
+                )
         return op_handle
 
-    @_with_state_lock
     def fetch(
         self,
         keys: Collection[BlockKey],
@@ -469,62 +461,65 @@ class _KVCRCore:
         self._next_op_handle += 1
         local_dram = self._local_dram
         ordered_keys = tuple(dict.fromkeys(keys))
-        if local_dram is None:
-            self._complete(
-                op_handle,
-                {key: OpEntryResult(OpEntryStatus.FAILED) for key in ordered_keys},
-            )
-            return op_handle
+        with self._state_lock:
+            if local_dram is None:
+                self._complete(
+                    op_handle,
+                    {key: OpEntryResult(OpEntryStatus.FAILED) for key in ordered_keys},
+                )
+                return op_handle
 
-        sources = {}
-        for key in ordered_keys:
-            if self._is_local_resident(key):
-                continue
-            if self._g3 is not None and self._g3.is_ready(key):
-                sources[key] = CacheTier.G3
-            elif request_id is not None and self._remote_fw_dram.query(key, request_id):
-                sources[key] = CacheTier.REMOTE_G2
-        deadline = self._operation_deadline()
-        destinations = local_dram.fetch(
-            op_handle,
-            ordered_keys,
-            sources,
-            request_id,
-            deadline,
-            hints=hints,
-            layout=expected_layout,
-        )
-        for source in (CacheTier.G3, CacheTier.REMOTE_G2):
-            self._start_local_fill(
-                source,
-                {
-                    key: destination
-                    for key, destination in destinations.items()
-                    if sources[key] is source
-                },
+            sources = {}
+            for key in ordered_keys:
+                if self._is_local_resident(key):
+                    continue
+                if self._g3 is not None and self._g3.is_ready(key):
+                    sources[key] = CacheTier.G3
+                elif request_id is not None and self._remote_fw_dram.query(
+                    key, request_id
+                ):
+                    sources[key] = CacheTier.REMOTE_G2
+            deadline = self._operation_deadline()
+            destinations = local_dram.fetch(
+                op_handle,
+                ordered_keys,
+                sources,
                 request_id,
                 deadline,
+                hints=hints,
+                layout=expected_layout,
             )
+            for source in (CacheTier.G3, CacheTier.REMOTE_G2):
+                self._start_local_fill(
+                    source,
+                    {
+                        key: destination
+                        for key, destination in destinations.items()
+                        if sources[key] is source
+                    },
+                    request_id,
+                    deadline,
+                )
         return op_handle
 
-    @_with_state_lock
     def release(self, handles: Collection[ReleaseHandle]) -> list[ReleaseResult]:
         if self._local_dram is None:
             return [(handle, False) for handle in handles]
-        return self._local_dram.release(handles)
+        with self._state_lock:
+            return self._local_dram.release(handles)
 
     # TODO: Expose individual entry completions as they become available.
-    @_with_state_lock
     def poll_completed(self) -> Iterable[OpResult]:
         self._progress.raise_if_failed()
         self._notify_transfer_errors(self._progress.take_completed())
         progress_items = self._pending_progress_items
         self._pending_progress_items = []
-        if self._g3 is not None:
-            progress_items = self._g3.poll_main(progress_items)
-        if self._local_dram is not None:
-            progress_items = self._local_dram.poll_main(progress_items)
-        self._remote_fw_dram.poll_main(progress_items)
+        with self._state_lock:
+            if self._g3 is not None:
+                progress_items = self._g3.poll_main(progress_items)
+            if self._local_dram is not None:
+                progress_items = self._local_dram.poll_main(progress_items)
+            self._remote_fw_dram.poll_main(progress_items)
         completed = self._completion_queue
         self._completion_queue = []
         return completed
@@ -548,24 +543,24 @@ class _KVCRCore:
         # TODO: Implement best-effort cancellation for fetch and deliver entries.
         return False
 
-    @_with_state_lock
     def get_stats(self) -> TelemetryStats | None:
         self._progress.raise_if_failed()
         stats = self._stats
         if stats is None:
             return None
-        resources = {
-            "block_records": len(self._block_record_map),
-            "in_flight_ops": self._outstanding_operations,
-            "framework_pins": len(self._framework_pin_keys),
-            "pinned_keys": sum(map(len, self._framework_pin_keys.values())),
-            "connected_remotes": self._remote_fw_dram._connected_remote_count,
-            "completed": len(self._completion_queue),
-        }
-        if self._local_dram is not None:
-            resources.update(self._local_dram.telemetry_state())
-        if self._g3 is not None:
-            resources.update(self._g3.telemetry_state())
+        with self._state_lock:
+            resources = {
+                "block_records": len(self._block_record_map),
+                "in_flight_ops": self._outstanding_operations,
+                "framework_pins": len(self._framework_pin_keys),
+                "pinned_keys": sum(map(len, self._framework_pin_keys.values())),
+                "connected_remotes": self._remote_fw_dram._connected_remote_count,
+                "completed": len(self._completion_queue),
+            }
+            if self._local_dram is not None:
+                resources.update(self._local_dram.telemetry_state())
+            if self._g3 is not None:
+                resources.update(self._g3.telemetry_state())
         for resource, value in resources.items():
             stats.set_gauge(STATE_METRIC, value, (resource,))
         self._stats = self._stats_factory() if self._stats_factory else None
